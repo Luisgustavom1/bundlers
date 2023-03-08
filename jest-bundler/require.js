2ba9c4851c63b4a43a98f17c1c954cbc98c5d4a0
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-03-07 21:24:00 -0300

    feat: add a runtime in require.js

---
+++ jest-bundler/require.js
@@ -0,0 +1,27 @@
+const modules = new Map();
+const define = (name, moduleFactory) => {
+    modules.set(name, moduleFactory);
+}
+
+const moduleCache = new Map();
+const requireModule = (name) => {
+    if (moduleCache.has(name)) {
+        return modules.get(name).exports;
+    }
+
+    if (!modules.has(name)) {
+        throw new Error(`Module ${name} does not exists`);
+    }
+
+    const moduleFactory = modules.get(name);
+
+    const module = {
+        exports: {},
+    };
+
+    moduleCache.set(name, module);
+
+    moduleFactory(module, module.exports, requireModule);
+    
+    return module.exports;
+}
