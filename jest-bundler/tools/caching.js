6b5f0f3d5d813ffc873cb522945d111d288605b7
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-03-11 22:06:50 -0300

    feat: caching modules

---
+++ jest-bundler/tools/caching.js
@@ -0,0 +1,23 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import md5 from 'crypto-js/md5.js';
+import { DIR, fromRootDir } from "../dir.js";
+
+
+export function writeCache(moduleName) {
+    try {
+        const contentCached = fs.readFileSync(fromRootDir(DIR.CACHE, md5(moduleName).toString()), 'utf8');
+        return contentCached;
+    } catch {
+        return undefined;
+    }
+}
+
+export function writeCache(moduleName, content) {
+    const cacheDir = fromRootDir(DIR.CACHE);
+
+    if (!fs.existsSync(cacheDir)) {
+        fs.mkdirSync(cacheDir);
+    }
+    fs.writeFileSync(path.join(cacheDir, md5(moduleName).toString()), content, 'utf-8');
+}
