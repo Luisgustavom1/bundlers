f9d2f4e632f1f86df4b851dd9eda4f62e4a0bfaa
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-03-15 09:03:39 -0300

    feat: generate source map when minify the code

--- jest-bundler/index.js
+++ jest-bundler/index.js
@@ -7,38 +7,51 @@ import { ARGS } from './tools/cli.js';
 import { generateModuleMap } from './tools/moduleMap.js';
 import { generateBundle } from './tools/bundler.js';
 import { DIR, fromRootDir } from './dir.js';
+import path from 'node:path';
 
-const start = hrtime.bigint();
+(async () => {
+    const start = hrtime.bigint();
 
-console.log(chalk.bold(`- 🔥🔥🔥 Building ${chalk.yellowBright(ARGS.entryPoint)}`));
-const modules = await generateModuleMap(ARGS.entryPoint);
+    console.log(chalk.bold(`- 🔥🔥🔥 Building ${chalk.yellowBright(ARGS.entryPoint)}`));
+    const modules = await generateModuleMap(ARGS.entryPoint);
 
-const worker = new Worker(
-    new URL(fromRootDir(DIR.TOOLS, 'worker.cjs'), import.meta.url),
-    {
-        enableWorkerThreads: true
+    const worker = new Worker(
+        new URL(fromRootDir(DIR.TOOLS, 'worker.cjs'), import.meta.url),
+        {
+            enableWorkerThreads: true
+        }
+    )
+
+    console.log(chalk.bold("\n- Serializing bundle"));
+    const results = await generateBundle(modules, worker.transformESMFiles);
+
+    const output = [
+        fs.readFileSync(fromRootDir(DIR.TOOLS, 'runtime.js'), 'utf-8'),
+        ...results,
+        `requireModule(0);`
+    ]
+
+    const codeOutput = {
+        code: output.join('\n'),
+        map: undefined
+    };
+
+    if (ARGS.minify) {
+        const minifiedCode = await minify(codeOutput.code, { sourceMap: true });
+
+        codeOutput.code = minifiedCode.code;
+        codeOutput.map = minifiedCode.map;
+    } 
+
+    if (ARGS.output) {
+        fs.writeFileSync(path.join('dist', ARGS.output), codeOutput.code, 'utf-8');
+
+        if (!codeOutput.map) return;
+
+        fs.writeFileSync(`${path.join('dist', ARGS.output)}.map`, codeOutput.map, 'utf-8');
     }
-)
 
-console.log(chalk.bold("\n- Serializing bundle"));
-const results = await generateBundle(modules, worker.transformESMFiles);
-
-const output = [
-    fs.readFileSync(fromRootDir(DIR.TOOLS, 'runtime.js'), 'utf-8'),
-    ...results,
-    `requireModule(0);`
-]
-
-let codeOutput = output.join('\n');
-
-if (ARGS.minify) {
-    codeOutput = (await minify(codeOutput, { compress: true, mangle: true })).code;
-} 
-
-if (ARGS.output) {
-    fs.writeFileSync(ARGS.output, codeOutput, 'utf-8');
-}
-
-worker.end();
-const end = hrtime.bigint();
-console.log(chalk.green(`Generate build successfully in ${end - start} nanoseconds`));
+    worker.end();
+    const end = hrtime.bigint();
+    console.log(chalk.green(`Generate build successfully in ${end - start} nanoseconds`));
+})()
